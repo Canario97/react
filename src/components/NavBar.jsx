acdb7d1e97af0f35f374ecb178b6825a754cc042
Canario907 <ulirest@gmail.com>
2023-06-15 15:31:05 -0300

    react

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export const NavBar = () => (
+    <header>
+        <nav>
+        <h6>MY ECOMMERCE</h6>
+            <ul>
+                <li>
+                    <a href="*">HOME</a>
+                    <a href="*">PRODUCTOS</a>
+                    <a href="*">CONTACTO</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+)
